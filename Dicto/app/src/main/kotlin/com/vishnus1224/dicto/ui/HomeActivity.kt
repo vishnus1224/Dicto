7122f3b3252f43204eabe5c22883064d59899f5e
Vishnu Shirodkar <vishnus1224@gmail.com>
2017-03-04 16:44:20 +0530

    moved to kotlin directory

---
+++ Dicto/app/src/main/kotlin/com/vishnus1224/dicto/ui/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.vishnus1224.dicto.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.vishnus1224.dicto.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
