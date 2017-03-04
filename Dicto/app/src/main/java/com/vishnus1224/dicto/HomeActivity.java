e80c0bc44c3d7847e2807304aac6c93b48db48d1
Vishnu Shirodkar <vishnus1224@gmail.com>
2017-03-04 15:52:41 +0530

    project commit

---
+++ Dicto/app/src/main/java/com/vishnus1224/dicto/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.vishnus1224.dicto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
