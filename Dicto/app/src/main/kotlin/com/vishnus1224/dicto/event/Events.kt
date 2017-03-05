d1a9ecd903bc04ad596d61b87714ad0c53b1685f
Vishnu Shirodkar <vishnus1224@gmail.com>
2017-03-05 17:53:03 +0530

    copy event detection

---
+++ Dicto/app/src/main/kotlin/com/vishnus1224/dicto/event/Events.kt
@@ -0,0 +1,49 @@
+package com.vishnus1224.dicto.event
+
+import android.content.ClipboardManager
+import android.content.Context
+import io.reactivex.Observable
+import android.content.Context.CLIPBOARD_SERVICE
+
+/**
+ * Exposes observables for system wide events.
+ * e.g. User copied text in some application.
+ * Created by Vishnu on 3/5/2017.
+ */
+
+/**
+ * Whenever user copies text from any of the apps and if the event has not already been emitted i.e it is unique,
+ * then observers will get notified of this event.
+ */
+fun copyText(context: Context) : Observable<String> {
+
+    val event : Observable<String> =  Observable.create { observer ->
+
+        val clipBoard = context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+
+        fun primaryClipChangedListener(){
+
+            //get the first copied item from clipboard
+            val copiedText = clipBoard.primaryClip.getItemAt(0)
+
+            if(!observer.isDisposed) {
+
+                observer.onNext(copiedText.text.toString())
+
+            }
+        }
+
+        clipBoard.addPrimaryClipChangedListener { primaryClipChangedListener() }
+
+        //when subscription is disposed, remove the listener.
+        observer.setCancellable { clipBoard.removePrimaryClipChangedListener { primaryClipChangedListener() } }
+
+    }
+
+    return event.distinct()
+
+}
+
+
+
+
